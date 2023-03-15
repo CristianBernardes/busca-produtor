8f49377e86e32b87bb5664e5110246f9b5ab93df
Cristian Anderson Oliveira Bernardes <cristian.aob@gmail.com>
2023-03-15 08:59:20 -0300

    Update ProducerRepository.php

--- app/Repositories/ProducerRepository.php
+++ app/Repositories/ProducerRepository.php
@@ -35,17 +35,34 @@ class ProducerRepository extends AbstractRepository
         string $minDistance = null,
         string $maxDistance = null
     ) {
-        return $this->model()::select(
-            '*',
-            DB::raw('ROUND(6371 * acos(cos(radians(' . $user->latitude . ')) * cos(radians(latitude)) * cos(radians(longitude) - radians(' . $user->longitude . ')) + sin(radians(' . $user->latitude . ')) * sin(radians(latitude))), 2) AS distance')
-        )->when(!$user->is_admin, function ($query) use ($maxDistance) {
+
+        if ($user->is_admin) {
+
+            $producers =  $this->model()::select(
+                '*',
+                DB::raw('ROUND(6371 * acos(cos(radians(' . $user->latitude . ')) * cos(radians(latitude)) * cos(radians(longitude) - radians(' . $user->longitude . ')) + sin(radians(' . $user->latitude . ')) * sin(radians(latitude))), 2) AS distance')
+            );
+        } else {
+
+            $producers =  $this->model()::select(
+                'id',
+                'producer_name',
+                'city',
+                'state',
+                'whatsapp_phone',
+                'volume_in_liters',
+                DB::raw('ROUND(6371 * acos(cos(radians(' . $user->latitude . ')) * cos(radians(latitude)) * cos(radians(longitude) - radians(' . $user->longitude . ')) + sin(radians(' . $user->latitude . ')) * sin(radians(latitude))), 2) AS distance')
+            );
+        }
+
+        return $producers->when(!$user->is_admin, function ($query) use ($maxDistance) {
             // limit distance for non-admin users
             return $query->having('distance', '<=', min($maxDistance ?? 500, 500));
         })->when($minDistance && $maxDistance && $minDistance <= $maxDistance, function ($query) use ($minDistance, $maxDistance) {
             // filter producers within the given distance range
             return $query->havingBetween('distance', [$minDistance, $maxDistance]);
         }, function ($query) {
-            // return all producers ordered by distance for admins
+            // return all producers ordered by distance
             return $query->orderBy('distance');
         })->get();
     }
