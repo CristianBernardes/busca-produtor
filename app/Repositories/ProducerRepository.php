50f1ab43480879a80bdb66ffe7819140b54d6d65
Cristian Anderson Oliveira Bernardes <cristian.aob@gmail.com>
2023-03-18 09:49:24 -0300

    Update ProducerRepository.php

--- app/Repositories/ProducerRepository.php
+++ app/Repositories/ProducerRepository.php
@@ -26,8 +26,8 @@ class ProducerRepository extends AbstractRepository
      * Searches for producers within a given distance range, optionally limited to a minimum and maximum distance.
      *
      * @param User $user The authenticated user.
-     * @param string|null $minDistance The minimum distance (in kilometers).
-     * @param string|null $maxDistance The maximum distance (in kilometers).
+     * @param null $minDistance The minimum distance (in kilometers).
+     * @param null $maxDistance The maximum distance (in kilometers).
      * @return mixed
      */
     public function searchProducersByLocation(
@@ -50,24 +50,23 @@ class ProducerRepository extends AbstractRepository
             $distance = [$newMinDistance, $newMaxDistance];
         }
 
+        $producers = $this->model()::select(
+            'id',
+            'producer_name',
+            'city',
+            'state',
+            'whatsapp_phone',
+            'volume_in_liters',
+        );
+
         if ($user->is_admin) {
 
-            $producers =  $this->model()::select(
-                '*',
-                DB::raw('ROUND(6371 * acos(cos(radians(' . $user->latitude . ')) * cos(radians(latitude)) * cos(radians(longitude) - radians(' . $user->longitude . ')) + sin(radians(' . $user->latitude . ')) * sin(radians(latitude))), 2) AS distance')
-            );
-        } else {
-
-            $producers =  $this->model()::select(
-                'id',
-                'producer_name',
-                'city',
-                'state',
-                'whatsapp_phone',
-                'volume_in_liters',
-                DB::raw('ROUND(6371 * acos(cos(radians(' . $user->latitude . ')) * cos(radians(latitude)) * cos(radians(longitude) - radians(' . $user->longitude . ')) + sin(radians(' . $user->latitude . ')) * sin(radians(latitude))), 2) AS distance')
-            );
+            $producers->addSelect('latitude', 'longitude');
         }
+
+        $producers->addSelect(
+            DB::raw('ROUND(6371 * acos(cos(radians(' . $user->latitude . ')) * cos(radians(latitude)) * cos(radians(longitude) - radians(' . $user->longitude . ')) + sin(radians(' . $user->latitude . ')) * sin(radians(latitude))), 2) AS distance')
+        );
 
         return $producers->when(!$user->is_admin, function ($query) use ($newMaxDistance) {
 
